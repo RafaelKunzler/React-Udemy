9b43133f9a9e064ed87ef4649d7630e284f01b51
Rafael Rodrigues <rafaelkunzler@gmail.com>
2023-01-31 11:53:37 -0300

    props e destructuring

--- 3_AVANCANDO_NO_REACT/avancando/src/App.js
+++ 3_AVANCANDO_NO_REACT/avancando/src/App.js
@@ -1,16 +1,27 @@
 import ManageData from './components/ManageData';
 import ListRender from './components/ListRender';
 import ConditionalRender from './components/ConditionalRender';
+import ShowUserName from './components/ShowUserName';
+import CarDetails from './components/CarDetails';
+
+import { useState } from 'react';
 
 import './App.css';
 
 function App() {
+  const name = "Joaquim"
+  const [userName] = useState("Maria")
+
   return (
     <div className="App">
       <h1>Avançando em React</h1>      
       <ManageData />
       <ListRender />
       <ConditionalRender />
+      {/* props */}
+      <ShowUserName name={userName} />
+      {/* destructuring */}
+      <CarDetails brand="VW" km={100000} color="Azul"/>
     </div>
   );
 }
