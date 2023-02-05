b60f278a631a28bf21304fcb041e1c7289adc0cb
Rafael Rodrigues <rafaelkunzler@gmail.com>
2023-02-05 17:07:49 -0300

    css de componentes

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 function App() {
   return (
     <div className="App">
+      {/* css global */}
       <h1>React com CSS</h1>
+      <MyComponent />
+      <p>Este paragrafo é o app.js</p>
     </div>
   );
 }
