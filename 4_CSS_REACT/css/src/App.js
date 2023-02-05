7d5a84a8772a925e64c8719afb41cc077aa0bdf6
Rafael Rodrigues <rafaelkunzler@gmail.com>
2023-02-05 17:19:40 -0300

    inline dinamico

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
 import MyComponent from './components/MyComponent';
 
+import {useState} from 'react';
+
 function App() {
+
+  const n = 15;
+  const [name] = useState("Rafael")
+
   return (
     <div className="App">
       {/* css global */}
@@ -15,6 +21,13 @@ function App() {
       <p style={{color: "blue", padding: "25px", borderTop: "2px solid red"}}>
         Este elemento foi estilizado de forma inline
       </p>
+      {/* inline css dinamico */}
+      <h2 style={n > 10 ? ({color: "purple"}) : ({color: "pink"})}>
+        css dinamico
+      </h2>
+      <h2 style={name === "Rafael" ? ({color: "green", backgroundColor: "#000"}) : (null)}>
+        Teste nome      
+      </h2>
     </div>
   );
 }
