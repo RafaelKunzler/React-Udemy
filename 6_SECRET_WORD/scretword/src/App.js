3fff7c260f42c0454ee0abb864bb9e42bea9228b
Rafael Rodrigues <rafaelkunzler@gmail.com>
2023-02-09 10:23:22 -0300

    processando letras validas e invalidas

--- 6_SECRET_WORD/scretword/src/App.js
+++ 6_SECRET_WORD/scretword/src/App.js
@@ -67,8 +67,30 @@ function App() {
 
   // process letter input
   const verifyLetter = (letter) => {
-    console.log(letter);
-  }
+    
+    const normalizedLetter = letter.toLowerCase();
+
+    //check if letter has already been utilized
+    if(guessedLetters.includes(normalizedLetter) || wrongLetters.includes(normalizedLetter)){
+      return
+    }
+
+    // push guessed letters or remove a guess
+    if(letters.includes(normalizedLetter)){
+      setGuessedLetters((actualGuessedLetters) => [
+        ...actualGuessedLetters,
+        normalizedLetter,
+      ]);
+    } else {
+      setWrongLetters((actualWrongLetters) => [
+        ...actualWrongLetters,
+        normalizedLetter,
+      ]);
+    }    
+  };
+
+    console.log(guessedLetters)
+    console.log(wrongLetters)
 
   // restart game
   const retry = () => {
