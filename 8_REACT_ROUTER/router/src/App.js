b8b06c100e9794467548edb93ce4e7cfc08a9446
Rafael Rodrigues <rafaelkunzler@gmail.com>
2023-02-17 11:50:01 -0300

    nested route

--- 8_REACT_ROUTER/router/src/App.js
+++ 8_REACT_ROUTER/router/src/App.js
@@ -12,6 +12,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home';
 import About from './pages/About';
 import Product from './pages/Product';
+import Info from './pages/Info';
 
 const url = "http://localhost:3000/products"
 
@@ -27,8 +28,11 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
+            {/* 6 - nested route*/}
+            <Route path='/products/:id/info' element={<Info />} />
             {/* 4 - rota dinamica*/}
             <Route path='/products/:id' element={<Product />} />
+
           </Routes>
       </BrowserRouter>
     </div>
